5fd631f621231ff4bc99199d9d4259bef5f772fd
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2020-04-18 14:45:07 -0400

    added solution

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
 
 // Code your solution here
+function findMatching(drivers, name){
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(driver => {
+    return driver.startsWith(string)
+  })
+}

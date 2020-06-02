ed4f75442ac26b80752c7ed25f8195381fab569f
mertoz41 <mertozkaynak@windowslive.com>
2020-06-02 12:23:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
 
 // Code your solution here
+// let sensitive = [];
+// for (let i=0; i < drivers.length; i++){
+    //     sensitive.push(drivers[i].toLowerCase());
+    // }
+    // let match = sensitive.filter(driver => {
+        //     return driver === string;
+        // });
+        
+function findMatching(drivers,string) {
+    let filt = drivers.filter( x=> {
+        if (x.slice(1) === string.slice(1)) {
+            return x;
+        };
+        });
+        return filt;
+}
+function fuzzyMatch(drivers,string){
+    let filt = drivers.filter(x => {
+        if (x.slice(0,2) === string) {
+            return x;
+        }
+    });
+return filt;
+}
